d517a0bf8e02869fb20b3685015ac8fcd9fc6036
willen <willen.sesantos@gmail.com>
2022-04-06 15:37:39 -0300

    inclusao mensagem de alerta

--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
-Conversor de moedas para real, projeto usando html, css e JavaScript com consumo de API 
+## Descrição
+
+Neste projeto temos um conversor de moedas estrangeiras para real. Para sua construção foram utilizadas as seguintes tecnologias: html css e javascript. Para se obter a cotação atualizada dass moedas trabalhada, o projeto faz o consumo da api do site economia.awesomeapi.com.br. Para obter a lista completa com os dados em em formato json <a href='https://economia.awesomeapi.com.br/json/all'>Clique aqui</a>.
+
+Para visualizar o projeto completo, <a href='https://willen01.github.io/conversor-de-moedas/'>Clique aqui</a>
+
+Página do projeto:
+![conv](https://user-images.githubusercontent.com/95586829/162043232-1a8ac2ee-34ac-4c1d-aae1-37014f9bcce6.png)
+
+
--- script.js
+++ script.js
@@ -6,20 +6,35 @@ let result = document.getElementById('res');
 let leg = document.getElementById('leg');
 
 
-async function change(value) {
+async function change(opcaoSelecionada) {
     let api = fetch(url)
-    let a = await api.then(res => res.json());
-    for (let key in a) {
-        if (value == key) {
-            btn.onclick = function () {
-                let rate = a[key].bid;
-                let name = a[key].name.replace("/Real Brasileiro", " para Real")
-                let valor = entrada.value
-                let res = rate * valor
+    let dados = await api.then(res => res.json());
 
-                leg.innerHTML = name;
-                result.innerHTML = `${res.toFixed(2)} Reais`
+    for (let moeda in dados) {
+        if (opcaoSelecionada == moeda) {
+            btn.onclick = function () {
+                let rate = dados[moeda].bid;
+                let nome = dados[moeda].name.replace('/Real Brasileiro', ' para Real:');
+                let valor = entrada.value;
+                let res = rate * valor;
+
+                if (!entrada.value || opt.options[opt.selectedIndex].value == 'NOT') {
+                    alert('selecione uma moeda ou valor para converter 😉');
+                } else {
+                    leg.innerHTML = nome;
+
+                    let num = `R$ ${res.toFixed(2)} Reais`;
+
+                    result.innerHTML = num.replace('.', ',');
+                }
             }
-        } 
+        }
     }
 }
+
+btn.onclick = function () {
+    if (opt.options[opt.selectedIndex].value == 'NOT') {
+        alert('selecione uma moeda ou valor para converter 😉');
+    }
+}
+
